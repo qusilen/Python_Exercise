18b527bff4f00342a29d44f0cbb2d61edf5ae1cb
Esra Özkutlu <87521424+qusilen@users.noreply.github.com>
2022-08-14 23:20:13 +0300

    Listenin ilk ve son elemanını yazdırma.

---
+++ List Ends.py
@@ -0,0 +1,15 @@
+"""
+Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) 
+and makes a new list of only the first and last elements of the given list. 
+For practice, write this code inside a function.
+"""
+a = [5, 10, 15, 20, 25, 30]
+
+def newlist():
+    print(a[0], a[-1])
+    
+newlist()
+
+
+
+    
